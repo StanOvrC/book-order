93fe1876d34ff409a2c6f1f605cbcf1c5c6fba9d
Stan <sls2003sls@gmail.com>
2025-11-27 21:40:06 +0300

    Add UserService and user dtos

---
+++ src/main/java/com/rsoi/service/dto/user/UserRegisterDto.java
@@ -0,0 +1,23 @@
+package com.rsoi.service.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegisterDto {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
